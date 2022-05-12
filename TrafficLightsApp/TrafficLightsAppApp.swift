bbe60794ebda605b90bc407c23e920982001e60c
Salnik Valeriia <melfed.salnik@gmail.com>
2022-05-12 17:12:02 +0200

    Initial Commit

---
+++ TrafficLightsApp/TrafficLightsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightsAppApp.swift
+//  TrafficLightsApp
+//
+//  Created by Валерия Смецкая on 12.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
